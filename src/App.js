12238ab8395d4d7c5a4106ebc23b78def8901ca6
Lakshmiit <lscomputercoachingcenter@gmail.com>
2024-12-15 18:36:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,11 @@ import BuyProductView from './BuyProductView';
 import AdminProductApproval from './AdminProductApproval.js';
 import AdminProductList from './AdminProductList.js';
 import AdminUpdateProduct from './AdminUpdateProduct.js';
-
+import Notifications from './Notifications.js';
+import RaiseTicketActionView from './RaiseTicketActionView.js';
+import RaiseaQuote from './RaiseaQuote.js';
+import RaiseTicketNotifications from './RaiseTicketNotifications.js';
+import AddTechnician from './AddTechnician.js';
 
 function App() {
   return (
@@ -27,23 +31,25 @@ function App() {
         {/* Main content */}
         <main className="container py-3 mt_100px">
           <Routes>
-            <Route path="/product/:productownedby" element={<UploadForm />} />
+            <Route path="/product/:productownedby/:userType" element={<UploadForm />} />
             {/* Dynamic product ID route for ProductView */}
-            <Route path="/product-view/:id/:productownedby" element={<ProductView />} /> 
+            <Route path="/product-view/:id/:productownedby/:userType" element={<ProductView />} /> 
             <Route path="/product-list/:productownedby" element={<ProductList />} />
-            <Route path="/product-edit/:id" element={<EditUploadForm />} />           
-            <Route path="/RaiseTicket/:customerId" element={<RaiseTicket />} />
-            <Route path="/buyProducts" element={<BuyProduct />} />
-            <Route path="/sidebar" element={<Sidebar />} />
-            <Route path="/buyproduct-view" element={<BuyProductView />} />
-            <Route path="/adminProductApproval/:id" element={<AdminProductApproval />} />
+            <Route path="/product-edit/:id/:userType" element={<EditUploadForm />} />           
+            <Route path="/RaiseTicket/:customerId/:userType" element={<RaiseTicket />} />
+            <Route path="/buyProducts/:userType" element={<BuyProduct />} />
+            <Route path="/sidebar/:userType" element={<Sidebar />} />
+            <Route path="/buyproduct-view/:userType" element={<BuyProductView />} />
+            <Route path="/adminProductApproval/:id/:userType" element={<AdminProductApproval />} />
             <Route path="/adminProductList" element={<AdminProductList />} />
             <Route path="/adminUpdateProduct/:id" element={<AdminUpdateProduct />} />
-          
+            <Route path="/notifications" element={<Notifications />} />
+            <Route path="/raiseTicketActionView" element={<RaiseTicketActionView />} />
+            <Route path="/raiseQuote/:userType" element={<RaiseaQuote />} />
+            <Route path="/raiseTicketNotification" element={<RaiseTicketNotifications />} />
+            <Route path="/addTechnician/:userType" element={<AddTechnician />} />
           </Routes>
         </main>
-
-      
       </div>
     </Router>
   );
