2243a125d12578e3bd3dc71eced39af6b6d8e2ca
Lakshmiit <lscomputercoachingcenter@gmail.com>
2024-12-15 23:08:13 +0530

    Update Sidebar.js

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -14,6 +14,7 @@ import RequestQuoteIcon from '@mui/icons-material/RequestQuote';
 import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
 import TransferWithinAStationIcon from '@mui/icons-material/TransferWithinAStation';
 import { useParams } from 'react-router-dom';
+
 // Sidebar component
 const Sidebar = ({ userType }) => { 
   const menuConfig = {
