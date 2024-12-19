48f6555d903a512700b962b7013a50a5699edb36
Lakshmiit <lscomputercoachingcenter@gmail.com>
2024-12-19 22:02:42 +0530

    Update EditUploadForm.js

--- src/EditUploadForm.js
+++ src/EditUploadForm.js
@@ -174,7 +174,7 @@ const ProductUpload = () => {
             formData.append('file', new Blob([byteArray], { type: mimeType }), fileName);
             formData.append('fileName', fileName);
 
-            const response = await fetch('https://handymanapiservices.azurewebsites.net/api/FileUpload/upload?filename=' + fileName, {
+            const response = await fetch('https://handymanapiv2.azurewebsites.net/api/FileUpload/upload?filename=' + fileName, {
                 method: 'POST',
                 headers: { 'Accept': 'text/plain' },
                 body: formData
