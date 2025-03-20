be7cc2c468d0232933112bf48edfc85fba06725b
Yasaswani <lscomputercoachingcenter@gmail.com>
2025-03-20 12:52:55 +0530

    Profile change

--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import Device from './Device.js';
 import NotificationBell from "./NotificationsBell";
 import OrdersNotificationBell from "./OrdersBellNotifications";
 import TrackStatusNotificationBell from "./TrackStatusBellNotifications";
@@ -810,6 +811,7 @@ const fetchImageUrl = async (photoId) => {
                 {/* </>
                 )} */}
               <div className="ticket-container">
+              <Device />
                 <div className="ticket-header">
                 <h4 className="ticket-title">My Tickets</h4>
                 {/* <h4 className="ticket-title">View All</h4> */}
