3bf54f8ea0378ec52b51fca15fc062d04e375cd1
Yasaswani <lscomputercoachingcenter@gmail.com>
2025-08-30 20:28:27 +0530

    Added text in Logo Page

--- src/HandyManLogo.js
+++ src/HandyManLogo.js
@@ -1,81 +1,75 @@
-import React, { useEffect, useRef, useState } from "react";
+// import React, {useEffect, useRef } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
-import HandyManCharacter from "./img/hm_char.png";
-import HandyManLogo from "./img/Hm_Logo 1.png";
-import { useNavigate } from 'react-router-dom';
+// import HandyManCharacter from "./img/hm_char.png";
+// import HandyManLogo from "./img/Hm_Logo 1.png";
+// import { useNavigate } from 'react-router-dom';
 
 const LandingPage = () => {
-  const navigate = useNavigate();
-  const wordRefs = useRef([]);
-  const [platform, setPlatform] = useState("Detecting...");
+  // const navigate = useNavigate();
 
-  useEffect(() => {
-    // Detect platform
-    const ua = navigator.userAgent || navigator.vendor || window.opera;
-    if (/iPad|iPhone|iPod/.test(ua) && !window.MSStream) {
-      setPlatform("iOS Device");
-      alert("Running on iOS");
-    } else if (/android/i.test(ua)) {
-      setPlatform("Android Device");
-     alert("Running on Android");
-    } else {
-      setPlatform("Other (Web/Desktop)");
-      alert("Running on Other Platform");
-    }
+  // const wordRefs = useRef([]);
 
-    // Word animations
-    wordRefs.current.forEach((word, index) => {
-      setTimeout(() => {
-        if (word) word.classList.add('show');
-      }, index * 1000);
-    });
+  // useEffect(() => {
+  //   wordRefs.current.forEach((word, index) => {
+  //     setTimeout(() => {
+  //       if (word) word.classList.add('show');
+  //     }, index * 1000);
+  //   });
 
-    // Safe navigation timeout
-    const totalDuration = wordRefs.current.length * 1500;
-    const redirectTimeout = setTimeout(() => {
-      alert("Redirecting to /loginnew...");
-      navigate('/loginnew');
-    }, totalDuration);
+  //   const totalDuration = wordRefs.current.length * 1500;
+  //   const redirectTimeout = setTimeout(() => {
+  //     //  navigate('/loginnew');
+  //   }, totalDuration);
 
-    return () => clearTimeout(redirectTimeout);
-  }, [navigate]);
+  //   return () => clearTimeout(redirectTimeout);
+  // }, [navigate]);
 
   return (
-    <div className="landing_page h-90 d-flex align-items-center py-2 flex-column mt-2">
-      <div className="spacer"></div>
-      <div className="w-auto p-4 text-center">
-        <div className="container">
-          <div className="row">
-            <div className="col">
-              <img src={HandyManCharacter} alt="Character" width="200" height="200" className="img-fluid"/>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col">
-              <img src={HandyManLogo} alt="Logo" width="190" height="90" className="img-fluid"/>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col">
-              <div id="ldr_txt" className="gry_fnt py-2">
-                <div ref={el => wordRefs.current[0] = el} className="word">• Your Home</div>
-                <div ref={el => wordRefs.current[1] = el} className="word">• Your Needs</div>
-                <div ref={el => wordRefs.current[2] = el} className="word">• Our Solutions</div>
-              </div>
-            </div>
-          </div>
-          {/* Debug Info */}
-          <div className="row mt-3">
-            <div className="col">
-              <small className="text-muted">Platform: {platform}</small>
-            </div>
+   <div className="landing_page h-90 d-flex align-items-center py-2 flex-column mt-2">
+  {/* <div className="spacer"></div> */}
+  <div className="w-auto p-4 text-center">
+    <div className="container">
+      {/* <div className="row">
+        <div className="col">
+          <img 
+            src={HandyManCharacter} 
+            alt="Character" 
+            width="200" 
+            height="200" 
+            className="img-fluid"
+          />
+        </div>
+      </div> */}
+      {/* <div className="row">
+        <div className="col">
+          <img 
+            src={HandyManLogo} 
+            alt="Logo" 
+            width="190" 
+            height="90" 
+            className="img-fluid"
+          />
+        </div>
+      </div> */}
+      <h1>Version 1.0</h1>
+      {/* <div className="row">
+        <div className="col">
+          <div id="ldr_txt" className="gry_fnt py-2">
+            <div ref={el => wordRefs.current[0] = el} className="word">• Your Home</div>
+            <div ref={el => wordRefs.current[1] = el} className="word">• Your Needs</div>
+            <div ref={el => wordRefs.current[2] = el} className="word">• Our Solutions </div>
           </div>
         </div>
-      </div>
-      <div className="spacer"></div>
+      </div> */}
     </div>
+  </div>
+  {/* <div className="spacer"></div> */}
+</div>
   );
 };
 
 export default LandingPage;
+
+
+
