1a282cacb411a6e6df5dfe04adf0e7e8ca515304
handymandev12 <ejasvith@gmail.com>
2026-07-15 03:36:56 +0100

    test

--- src/src/config.js
+++
@@ -1,7 +0,0 @@
-export const appConfig = {
-  apiBaseUrl: window.APP_CONFIG?.API_BASE_URL || "http://localhost:3000",      
-};
-
-
-// ${appConfig.apiBaseUrl}
-// https://lmartapiv1-fxcyd2b4btacgsav.westus2-01.azurewebsites.net
