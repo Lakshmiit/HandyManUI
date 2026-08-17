a0aa87ed7916b8a84d919c36cdff0fa3d695902d
Lakshmiit <lscomputercoachingcenter@gmail.com>
2026-08-17 22:29:54 +0530

    Update VendorOrdersPage.js

--- src/VendorOrdersPage.js
+++ src/VendorOrdersPage.js
@@ -18,7 +18,7 @@ import autoTable from "jspdf-autotable";
 import ImageCache from "./utils/ImageCache";
 import { getImageFilename, imageValueToUrl } from "./utils/imageSource";
 
-const API_BASE = "https://localhost:7091/api";
+const API_BASE = "https://apiqa-b5cyfzbhhah5adc9.westus2-01.azurewebsites.net/api";
 const GET_VENDOR_ORDERS = `${API_BASE}/Mart/GetVendorOrdersByVendorId`;
 const GET_ALL_DELIVERY_PARTNERS = `${API_BASE}/DeliveryPartner/GetAllDeliveryPartners`;
 const UPDATE_ORDER = `${API_BASE}/Mart/UpdateProductDetails`;
